e0c47eeec994b01e0e53835bbdc0139dd8b2037d
TheMannager <nathanielcmann@gmail.com>
2019-09-25 16:39:48 -0500

    Update calc.cpp

--- twenty48/calc.cpp
+++ twenty48/calc.cpp
@@ -8,29 +8,7 @@ using namespace std;
 // main function -
 int main()
 {
-  int numberOfCases;
-  // r = expected revunue no adverts
-  // e =  expected revunue with adverts
-  // c = cost of adverts
-  int r;
-  int e;
-  int c;
-  cin >> numberOfCases;
-  for (size_t i = 0; i < numberOfCases; i++) {
-    cin >> r;
-    cin >> e;
-    cin >> c;
-    if ((e-c)==r) {
-      cout << "does not matter";
-    }
-    if ((e-c)>r) {
-      cout << "advertise";
-    }
-    if ((e-c)<r) {
-      cout << "do not advertise";
-    }
-    cout <<'\n';
-  }
+  
 
 
 
